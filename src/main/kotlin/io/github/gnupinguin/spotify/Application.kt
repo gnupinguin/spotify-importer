b77479e4d0ebedc2b3203df381c82768bf607852
Ilja Pavlov <gnupinguin@yahoo.com>
2021-06-15 12:17:07 +0300

    Initial project configuration

---
+++ src/main/kotlin/io/github/gnupinguin/spotify/Application.kt
@@ -0,0 +1,15 @@
+package io.github.gnupinguin.spotify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Profile
+
+@EnableFeignClients
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+        runApplication<Application>(*args)
+}
+
